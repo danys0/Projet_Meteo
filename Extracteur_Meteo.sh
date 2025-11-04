c861c3641ce2f4253fc712dd6ed21f1dead4a5c2
Danys0 <danou1301413@gmail.com>
2025-11-04 22:03:25 +0100

    Extracteur_Meteo.sh

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+	echo "Erreur :  Aucun paramètre en entrée "
+	echo "Usage : ./Extracteur_Meteo.sh Ville"
+fi
+
 Ville="$1"
 
 fichier="./meteo_${Ville}.txt"
 
 curl -s https://wttr.in/${Ville} -o "$fichier"
+
