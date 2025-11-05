b32380efd770c00205e540964656c9e124be074e
geeeeeme <thomashuet1303@gmail.com>
2025-11-05 18:05:12 +0100

    Ajout étape 3

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 if [ -z "$1" ]; then
 	echo "Erreur :  Aucun paramètre en entrée "
 	echo "Usage : ./Extracteur_Meteo.sh Ville"
@@ -11,6 +10,3 @@ Ville="$1"
 fichier="./meteo_${Ville}.txt"
 
 curl -s https://wttr.in/${Ville} -o "$fichier"
-
-=======
->>>>>>> 344168cb5c9eeaec5549f0c5129024da124a05d4
