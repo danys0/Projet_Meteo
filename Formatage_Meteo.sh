839ea76ff15ae6372b5ac8ed100fbe7726654331
geeeeeme <thomashuet1303@gmail.com>
2025-11-07 15:05:45 +0100

    ajout de l'étape 3

--- Formatage_Meteo.sh
+++ Formatage_Meteo.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Erreur : Aucun paramètre en entrée"
     echo "Usage : $0 Ville"
     exit 1
 fi
 
 Ville="$1"
 
-# Récupérer directement la température actuelle et demain
-temps=$(curl -s "wttr.in/${Ville}?format=%t+%T")
-temp_actuelle=$(echo "$temps" | cut -d'+' -f1)
-temp_demain=$(echo "$temps" | cut -d'+' -f2)
+donnees_auj=$(curl -s "wttr.in/${Ville}?format=%t+%C+%w+%h")
+temp_actuelle=$(echo "$donnees_auj" | awk '{print $1}')
+condition_actuelle=$(echo "$donnees_auj" | awk '{print $2, $3}')
+vent_actuel=$(echo "$donnees_auj" | awk '{print $4}')
+humidite_actuelle=$(echo "$donnees_auj" | awk '{print $5}')
 
-# Écrire sur une seule ligne dans meteo.txt
-echo "Ville : $Ville | Température actuelle : $temp_actuelle | Prévision demain : $temp_demain" >> meteo.txt
-echo "Données formatées ajoutées dans meteo.txt"
+
+donnees_demain=$(curl -s "wttr.in/${Ville}?format=%m+%M+%w+%h&num_of_days=2")
+
+
+temp_min_demain=$(echo "$donnees_demain" | awk '{print $1}')
+temp_max_demain=$(echo "$donnees_demain" | awk '{print $2}')
+vent_demain=$(echo "$donnees_demain" | awk '{print $3}')
+humidite_demain=$(echo "$donnees_demain" | awk '{print $4}')
+condition_demain=$(echo "$donnees_demain" | awk '{print $5,$6}')
+
+
+echo "--- Météo actuelle pour $Ville ---"
+echo "Température : $temp_actuelle"
+echo "Condition   : $condition_actuelle"
+echo "Vent        : $vent_actuel"
+echo "Humidité    : $humidite_actuelle"
+
+echo "--- Prévision pour demain ---"
+echo "Température : $temp_min_demain à $temp_max_demain"
+echo "Condition   : $condition_demain"
+echo "Vent        : $vent_demain"
+echo "Humidité    : $humidite_demain"
 
