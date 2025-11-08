9a2737fedbb0c649a6dcf421cf7dacebdbe6cf83
Alex-droid-maker <Alexandre.gui2005@gmail.com>
2025-11-08 17:38:26 +0100

    Tentative de correction de bug de l'étape 3 : Réussi à moitié ( potentiel prise d'une mauvaise ligne dans wrrt.in ce qui empêche les prédictions pour demain

--- Formatage_Meteo.sh
+++ Formatage_Meteo.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 if [ -z "$1" ]; then
@@ -7,25 +8,31 @@ fi
 
 ville="$1"
 
-donnees_auj=$(curl -s "wttr.in/${Ville}?format=%t+%C+%w+%h")
+donnees_auj=$(curl -s "wttr.in/${ville}?format=%t+%C+%w+%h")
 temp_actuelle=$(echo "$donnees_auj" | awk '{print $1}')
-condition_actuelle=$(echo "$donnees_auj" | awk '{print $2, $3}')
-vent_actuel=$(echo "$donnees_auj" | awk '{print $4}')
-humidite_actuelle=$(echo "$donnees_auj" | awk '{print $5}')
+humidite_actuelle=$(echo "$donnees_auj" | awk '{print $NF}')
+vent_actuel=$(echo "$donnees_auj" | awk '{print $(NF-1)}')
+condition_actuelle=$(echo "$donnees_auj" | awk '{for(i=2;i<=NF-2;i++) printf $i " "; print ""}')
 
+donnees_demain=$(curl -s "wttr.in/${ville}?format=%m+%M+%w+%h+%C&num_of_days=2" | sed -n '2p')
 
-donnees_demain=$(curl -s "wttr.in/${Ville}?format=%m+%M+%w+%h&num_of_days=2")
-
-
-temp_min_demain=$(echo "$donnees_demain" | awk '{print $1}')
-temp_max_demain=$(echo "$donnees_demain" | awk '{print $2}')
-vent_demain=$(echo "$donnees_demain" | awk '{print $3}')
-humidite_demain=$(echo "$donnees_demain" | awk '{print $4}')
-condition_demain=$(echo "$donnees_demain" | awk '{print $5,$6}')
+if [ -z "$donnees_demain" ]; then
+    temp_min_demain="N/A"
+    temp_max_demain="N/A"
+    vent_demain="N/A"
+    humidite_demain="N/A"
+    condition_demain="N/A"
+else
+    temp_min_demain=$(echo "$donnees_demain" | awk '{print $1}')
+    temp_max_demain=$(echo "$donnees_demain" | awk '{print $2}')
+    vent_demain=$(echo "$donnees_demain" | awk '{print $(NF-1)}')
+    humidite_demain=$(echo "$donnees_demain" | awk '{print $NF}')
+    condition_demain=$(echo "$donnees_demain" | awk '{for(i=5;i<=NF-2;i++) printf $i " "; print ""}')
+fi
 
 fichier_sortie="meteo.txt"
 
-echo "--- Météo actuelle pour $Ville ---"
+echo "--- Météo actuelle pour $ville ---"
 echo "Température : $temp_actuelle"
 echo "Condition   : $condition_actuelle"
 echo "Vent        : $vent_actuel"
