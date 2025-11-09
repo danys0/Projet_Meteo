8c1a14194c65a4544de856c7daefd00693ee0670
Nina km <nina.kozlov100@gmail.com>
2025-11-09 16:57:21 +0100

    Version v1.1.0

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -11,3 +11,14 @@ fichier="./meteo_${Ville}.txt"
 
 curl -s https://wttr.in/${Ville} -o "$fichier"
 
+meteo="./meteo_${Ville}.txt"
+
+
+jour1et2=$(awk '
+/┌──────────────────────────────┬/ {block++}
+block >= 1 && block <= 2
+/└──────────────────────────────┴/ && block == 2 {exit}
+' "$meteo")
+
+echo "$jour1et2"
+
