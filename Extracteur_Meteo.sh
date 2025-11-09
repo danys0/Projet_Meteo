fb58dc482ae813f0e39983b5fa336509ad9143f4
Nina km <nina.kozlov100@gmail.com>
2025-11-09 17:12:43 +0100

    AJout de la version v1.1.0

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Erreur :  Aucun paramètre en entrée "
-	echo "Usage : ./Extracteur_Meteo.sh Ville"
+        echo "Erreur :  Aucun paramètre en entrée "
+        echo "Usage : ./Extracteur_Meteo.sh Ville"
 fi
 
 Ville="$1"
@@ -10,5 +10,14 @@ Ville="$1"
 fichier="./meteo_${Ville}.txt"
 
 curl -s https://wttr.in/${Ville} -o "$fichier"
-# il faut ajouter le script Extraction_Auj_Dem.sh
-#test tag
+
+meteo="./meteo_${Ville}.txt"
+
+
+jour1et2=$(awk '
+/┌──────────────────────────────┬/ {block++}
+block >= 1 && block <= 2
+/└──────────────────────────────┴/ && block == 2 {exit}
+' "$meteo")
+
+echo "$jour1et2"
