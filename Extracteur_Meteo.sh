8a8513a38cb50d045ab048cf59f2a4fa45831120
Nina km <nina.kozlov100@gmail.com>
2025-11-09 17:29:27 +0100

    Ajout de la version v2.0.0

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	echo "Erreur :  Aucun paramètre en entrée "
-	echo "Usage : ./Extracteur_Meteo.sh Ville"
-fi
-
 Ville="$1"
+if [ -z "$1" ] || [ "$1" -ne 1];
+then
+	ville="Toulouse"
+fi
 
 fichier="./meteo_${Ville}.txt"
 
 curl -s https://wttr.in/${Ville} -o "$fichier"
+
+meteo="./meteo_${Ville}.txt"
+
+
+jour1et2=$(awk '
+/┌──────────────────────────────┬/ {block++}
+block >= 1 && block <= 2
+/└──────────────────────────────┴/ && block == 2 {exit}
+' "$meteo")
+
+echo "$jour1et2"
+
 
